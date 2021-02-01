583be6d81352d1c8d44f69d51ba0232214a1ff5b
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-01 12:40:35 +0530

    Adding process.env.INITAL_TOKENS and require .env file

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,13 +1,12 @@
 var MyToken = artifacts.require("MyToken.sol");
 var MyTokenSale = artifacts.require("MyTokenSale");
 require("dotenv").config({path: "../.env"});
-console.log(process.env);
 
 module.exports = async function(deployer) { 
     let addr = await web3.eth.getAccounts();
-    await deployer.deploy(MyToken, 1000000);
+    await deployer.deploy(MyToken, process.env.INITAL_TOKENS);
     await deployer.deploy(MyTokenSale, 1, addr[0], MyToken.address);
     let instance = await MyToken.deployed();
-    await instance.transfer(MyTokenSale.address, 1000000);
+    await instance.transfer(MyTokenSale.address, process.env.INITAL_TOKENS);
 
 }; 
