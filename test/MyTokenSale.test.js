f19e51ad6b4aef368cb0b30ce4d343fbd8698886
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-01 13:25:42 +0530

    First test case to check the deployer account  should not have any tokens

---
+++ test/MyTokenSale.test.js
@@ -0,0 +1,18 @@
+const TokenSale = artifacts.require("MyTokenSale");
+const Token = artifacts.require("MyToken");
+
+const chai = require("./setupchai.js");
+const BN = web3.utils.BN;
+const expect = chai.expect;
+
+require("dotenv").config({path: "../.env"});
+
+contract("TokenSale Test", async(accounts) => {
+
+    const [deployerAccount, recipient, anotherAccount] = accounts;
+
+    it('should not have any tokens in my deployerAccount', async() => {
+        let instance = await Token.deployed();
+        return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
+    });
+});
