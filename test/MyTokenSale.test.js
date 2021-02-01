7c76cbce32e9c536555d0f24dcc6d9e4d1a11e1e
prathibhabilagi <31176835+prathibhabilagi@users.noreply.github.com>
2021-02-01 13:59:08 +0530

    Adding two test cases to check all tokens should be TokenSale contract and another test case to but tokens

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -15,4 +15,20 @@ contract("TokenSale Test", async(accounts) => {
         let instance = await Token.deployed();
         return expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0));
     });
+    
+    it("all tokens should be in the TokenSale Samrt Contract by default", async() => {
+        let instance = await Token.deployed();
+        let balanceOfTokenSaleSmartContract = await instance.balanceOf(TokenSale.address);
+        let totalSupply = await instance.totalSupply();
+        return expect(balanceOfTokenSaleSmartContract).to.be.a.bignumber.equal(totalSupply);
+    });
+
+    it("should be possible to buy tokens", async () => {
+        let tokenInstance = await Token.deployed();
+        let tokenSaleInstance = await TokenSale.deployed();
+        let balanceBefore = await tokenInstance.balanceOf.call(recipient);
+        expect(tokenSaleInstance.sendTransaction({from: recipient, value: web3.utils.toWei("1", "wei")})).to.be.fulfilled;
+        return expect(balanceBefore + 0).to.be.bignumber.equal(await tokenInstance.balanceOf.call(recipient));
+    });
+    
 });
